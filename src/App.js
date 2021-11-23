0b5270f60d54752ba8afff82c08f94041a7a1749
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-11-23 14:55:16 +0700

    Member & Guest Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from "./logo.svg";
+import { createBrowserHistory } from "history";
+import { Route, Router, Switch } from "react-router-dom";
 import "./assets/style.css";
 
+import { MemberRoute } from "components/Routes/MemberRoute";
+import { GuestRoute } from "components/Routes/GuestRoute";
+
+import Login from "pages/Login";
+import NotFound from "pages/404";
+
 function App() {
+  const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router history={history}>
+        <Switch>
+          <GuestRoute path="/login" component={Login} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
